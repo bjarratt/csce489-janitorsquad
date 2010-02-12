1f4506ed123f5a29e5a8656ecf3da7b5f821da40
heytherebrandon <heytherebrandon@77dfd326-079e-11df-bdae-5552c88b53c9>
2010-02-12 09:14:06 +0000

    NU SCREENZ

--- trunk/Screens/MainMenuScreen.cs
+++ trunk/Screens/MainMenuScreen.cs
@@ -27,7 +27,7 @@ namespace GameStateManagement
         /// Constructor fills in the menu contents.
         /// </summary>
         public MainMenuScreen()
-            : base("Main Menu")
+            : base("")
         {
             // Create our menu entries.
             MenuEntry playGameMenuEntry = new MenuEntry("Play Game");
